d666050f4460e1fd0a60ad292a5598853e99c5a4
ratomsky <max.work.dev@gmail.com>
2022-06-12 22:45:03 +0300

    feat: add cp operation

--- src/cli-operations/filesystem/cp.js
+++ src/cli-operations/filesystem/cp.js
@@ -1,21 +1,15 @@
-import { copyFile, readdir, mkdir } from "fs/promises";
+import { copyFile } from "fs/promises";
 import path from "path";
+import { TEXT_MESSAGES } from "../../const/message.js";
 
 export const cp = async (command_content, cli_config) => {
   try {
     const [path_to_file, path_to_new_directory] = command_content;
 
-    // await mkdir(files_copy);
-    // const files = await readdir(files_folder);
-
-    // for (const file of files) {
-    //   await copyFile(
-    //     path.join(files_folder, file),
-    //     path.join(files_copy, file)
-    //   );
-    // }
-
-    await copyFile(path.join(files_folder, file), path.join(files_copy, file));
+    await copyFile(
+      path.join(cli_config.directory, path_to_file),
+      path.join(cli_config.directory, path_to_new_directory)
+    );
   } catch (error) {
     throw new Error(TEXT_MESSAGES.COMMON_ERROR());
   }
